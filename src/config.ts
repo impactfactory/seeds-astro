d767f55e913072743c0cd63f0e8255fbf9fc959f
ImpactFactory <webmaster@impactfactory.ch>
2022-12-28 13:04:30 +0100

    start

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Seeds";
+export const SITE_DESCRIPTION = "Leben fördern";
