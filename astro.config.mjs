e23ba1d406d468d37d7dab2e591af209e7e572b1
ImpactFactory <webmaster@impactfactory.ch>
2023-01-07 15:13:59 +0100

    icons, picture component, breadcrums 2 levels

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import svelte from "@astrojs/svelte";
+//import svelte from "@astrojs/svelte";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
-import partytown from "@astrojs/partytown";
+//import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
